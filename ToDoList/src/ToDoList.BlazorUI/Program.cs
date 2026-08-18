ec9f3b037ed1d36cac585e63212336e0c86e1893
Bekzod <bekzodjonxasanov7@gmail.com>
2026-08-18 16:03:00 +0500

    added

--- ToDoList/src/ToDoList.BlazorUI/Program.cs
+++ ToDoList/src/ToDoList.BlazorUI/Program.cs
@@ -11,7 +11,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // API base address (from wwwroot/appsettings.json).  
-var apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? "http://46.224.208.19:5000/";
+var apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? "http://169.58.146.228:5000/";
 
 // Local storage for tokens.  
 builder.Services.AddBlazoredLocalStorage();
